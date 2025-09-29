af59b96a97f3d5edae21922ad38d39d4dc9c8a02
DeysiCruz <you@example.com>
2025-09-29 16:31:05 -0500

    new

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/repositorio/ParticipanteRepositorio.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/repositorio/ParticipanteRepositorio.java
@@ -1,11 +1,9 @@
 package pe.edu.upeu.asistencia.repositorio;
-
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import pe.edu.upeu.asistencia.enums.Carrera;
 import pe.edu.upeu.asistencia.enums.TipoParticipante;
 import pe.edu.upeu.asistencia.modelo.Participante;
-
 import java.util.ArrayList;
 import java.util.List;
 
